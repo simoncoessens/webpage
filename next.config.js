0370fd333a0ff358bf347d8eacc56ffe9acccfce
simoncoessens <56151036+simoncoessens@users.noreply.github.com>
2024-10-21 20:24:14 +0200

    push

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-	reactStrictMode: true,
-	output: "export", // Enable static site generation
-	basePath: isProd ? '/webpage' : '', // Use 'webpage' as the basePath
-	assetPrefix: isProd ? '/webpage/' : '',
-	images: {
-		unoptimized: true, // Disable image optimization for static export
-	},
+  output: 'export',
+  reactStrictMode: true,
+  basePath: isProd ? '/webpage' : '',
+  assetPrefix: isProd ? '/webpage/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
